1675a38c965d14535c5873637e68e45158cb001e
Ramino666 <62191394+Ramino69@users.noreply.github.com>
2022-01-01 22:35:38 +0000

    Slider added

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ st.write('''This is our first Web App.
 Enjoy it!''')
 st.write(df)
 st.line_chart(df)
+st.area_chart(df)
+
+myslider = st.slider('Celius')
+st.write(myslider, 'in Fahrenheit is', myslider * 9/5 + 32)
